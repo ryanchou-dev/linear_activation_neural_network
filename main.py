3168afa14c13070cffa3c3e35156b13edf178420
Ryan Chou <81596991+ryanchou-dev@users.noreply.github.com>
2021-05-26 14:32:13 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
-import tensorflow as tf 
 import numpy as np 
+import tensorflow as tf
 from tensorflow import keras 
 
 model = tf.keras.Sequential(
-    [keras.layers.Dense(units=1, input_shape=[1])]
-    )
+    [keras.layers.Dense(units=1, input_shape=[1])])
 
 model.compile(optimizer='sgd', loss='mean_squared_error')
 
-xs = np.array([1.0, 4.0, 3.0, 6.0, 23.0, 19.0], dtype=float)
-ys = np.array([11.0, 38.0, 4.0, 7.0, 24.0, 20.0], dtype=float)
+xs = np.array([1.0, 9.0, 2.0, 12.0, 32.0, 45.0], dtype=float)
+ys = np.array([2.0, 18.0, 4.0, 24.0, 64.0, 90.0], dtype=float)
 
-model.fit(xs, ys, epochs=100)
+model.fit(xs, ys, epochs=10)
 
-print(model.predict([10.0]))
+print(model.predict([90.0], batch_size=1))
 
 # y = 9x + 2
 
